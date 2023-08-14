4b27ba645e1240496ac4e7b27da01f84993f6919
Nicholas <rabalaond@gmail.com>
2023-08-14 15:54:10 +0200

    working on edit

--- app.js
+++ app.js
@@ -19,47 +19,20 @@ const db = admin.firestore();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-const students = [
-    {
-        name:'Nicholas',
-        surname:'Rabalao',
-        age:30,
-    },
-    {
-        name:'Vukona',
-        surname:'Mnisi',
-        age:31,
-    },
-    {
-        name:'Loy',
-        surname:'Nethutshelo',
-        age:28,
-    },
-];
-
-// Middleware 
-// app.use((req, res, next) => {
-//     console.log(`Hostname: ${req.hostname}`);
-//     console.log(`URL: ${req.path}`);
-//     console.log(`Method: ${req.method}`);
-//     next(); 
-// })
 app.use(morgan('common'));
 app.use(express.static('public'))
 
 // Read data
 async function fetchData(req, res, next) {
     try {
-        const jobRef = db.collection('jobs');
-        const response = await jobRef.get();
+        const employeeRef = db.collection('employees');
+        const response = await employeeRef.get();
         const responseData = []
-        response.forEach((job) => {
-            responseData.push({...job.data(), id:job.id});
+        response.forEach((employee) => {
+            responseData.push({...employee.data(), id:employee.id});
         })
         console.log(responseData);
-        req.body.jobs = responseData;
+        req.body.employees = responseData;
         next();
     } catch (error) {
         console.log(error.message);
@@ -67,10 +40,40 @@ async function fetchData(req, res, next) {
     }
 }
 
-app.get('/', fetchData, (req, res) => {
-    // res.sendFile('./views/index.html', {root:__dirname});
-    res.send(req.body.jobs);
-    res.render('index', {jobs: req.body.jobs, title:'Home'});
+app.get('/employees', fetchData, (req, res) => {
+    res.render('index', {employees: req.body.employees, title:'Home'});
+})
+
+app.post('/employees', async (req, res) => {
+    console.log('adding employee');
+    try {
+        const employee = {
+            fullName: req.body.fullName,
+            idNumber: req.body.idNumber,
+            email: req.body.email,
+            position: req.body.position,
+            phoneNumber: req.body.phoneNumber,
+            picture: "No picture",
+        }
+        const response = await db.collection('employees').add(employee);
+        // res.send(response)
+        res.redirect('/employees')
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+// Delete the post
+app.delete('/employees/:id', async(req, res) => {
+    console.log('trying to delete employee');
+    try {
+        const response = await db.collection('employees').doc(req.params.id).delete();
+        console.log('Delete successful');
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 })
 
 app.get('/about', (req, res) => {
@@ -80,35 +83,31 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add', {title:'Add'});
 })
-app.post('/add', async (req, res) => {
-    console.log('creating post');
+
+// Read data
+async function fetchEmployee(req, res, next) {
     try {
-        const job = {
-            title: req.body.title,
-            companyName: req.body.companyName,
-            location: req.body.location,
-            description: req.body.description,
-            time: admin.firestore.Timestamp.now(),
-        }
-        const response = await db.collection('jobs').add(job);
-        res.send(response)
+        const response = await db.collection('employees').doc(req.params.id).get();
+        const employee = {...response.data(), id: response.id}
+        console.log(response);
+        req.body.employee = employee;
+        next();
     } catch (error) {
-        console.log(error);
-        res.send(error);
+        console.log(error.message);
+        next();
     }
+}
+
+app.get('/edit/:id',fetchEmployee, (req, res) => {
+    // res.send(req.body.employee);
+    res.render('add', {title:'Edit', employee: req.body.employee});
 })
 
-// Delete the post
-app.delete('/delete/:id', async(req, res) => {
-    try {
-        const response = await db.collection('jobs').doc(req.params.id).delete();
-        console.log('Delete successful');
-        res.send(response)
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
+app.get('/edit', (req, res) => {
+    res.render('add', {title:'Edit', employee: req.body.employee});
 })
+
+
 
 app.use((req, res) => {
     res.status(200).render('404', {title:'Not Found'});
